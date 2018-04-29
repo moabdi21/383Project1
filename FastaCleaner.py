5f6d75efe8162ee862460f5e57d78eb07d6f4a79
unknown <moabdi21@gmail.com>
2018-04-28 23:18:21 -0700

    Update on Clean_raw 2017 & 2018, and some changes in script.

--- FastaCleaner.py
+++ FastaCleaner.py
@@ -1,22 +1,10 @@
 #Getting database file from: https://www.fludb.org/brc/home.spg?decorator=vipr
 
-#used by correctState fucntion
-states = {'Alabama':0,'Alaska':0,'Arizona':0,'Arkansas':0,'California':0,'Colorado':0,
-        'Connecticut':0,'Delaware':0,'Florida':0,'Georgia':0,'Hawaii':0,'Idaho':0, 
-        'Illinois':0,'Indiana':0,'Iowa':0,'Kansas':0,'Kentucky':0,'Louisiana':0,
-        'Maine':0, 'Maryland':0,'Massachusetts':0,'Michigan':0,'Minnesota':0,
-        'Mississippi':0, 'Missouri':0,'Montana':0,'Nebraska':0,'Nevada':0,
-        'New Hampshire':0,'New Jersey':0,'New Mexico':0,'New York':0,
-        'North Carolina':0,'North Dakota':0,'Ohio':0,    
-        'Oklahoma':0,'Oregon':0,'Pennsylvania':0,'Rhode Island':0,
-        'South  Carolina':0,'South Dakota':0,'Tennessee':0,'Texas':0,'Utah':0,
-        'Vermont':0,'Virginia':0,'Washington':0,'West Virginia':0,
-        'Wisconsin':0,'Wyoming':0}
 
 
 #Takes int he strain ID and make sure the that the strain is in the states/USA.
 #restricing the program to take a limited amount of strains from each state. 5 DNA strains for each state.
-def correctState(strainID):
+def correctState(strainID, states):
     #print(strainID)
     if(strainID.find('(A/') == -1):
         return False
@@ -54,11 +42,24 @@ def readFile(fileName):
     tempID = "" #fatsa strain ID
     is_location_USA = False
 
+    #used by correctState fucntion
+    states = {'Alabama':0,'Alaska':0,'Arizona':0,'Arkansas':0,'California':0,'Colorado':0,
+        'Connecticut':0,'Delaware':0,'Florida':0,'Georgia':0,'Hawaii':0,'Idaho':0, 
+        'Illinois':0,'Indiana':0,'Iowa':0,'Kansas':0,'Kentucky':0,'Louisiana':0,
+        'Maine':0, 'Maryland':0,'Massachusetts':0,'Michigan':0,'Minnesota':0,
+        'Mississippi':0, 'Missouri':0,'Montana':0,'Nebraska':0,'Nevada':0,
+        'New Hampshire':0,'New Jersey':0,'New Mexico':0,'New York':0,
+        'North Carolina':0,'North Dakota':0,'Ohio':0,    
+        'Oklahoma':0,'Oregon':0,'Pennsylvania':0,'Rhode Island':0,
+        'South  Carolina':0,'South Dakota':0,'Tennessee':0,'Texas':0,'Utah':0,
+        'Vermont':0,'Virginia':0,'Washington':0,'West Virginia':0,
+        'Wisconsin':0,'Wyoming':0}
+
     #read file
     print("reading file.....")
     for line in file:
         if line[0] == '>':
-            if(correctState(line)):
+            if(correctState(line, states)):
                 #adding the H3N2 strain to the strain ID(key) 
                 #print(len(strainDict))
                 is_location_USA = True
@@ -70,6 +71,38 @@ def readFile(fileName):
                 is_location_USA = False
         elif (is_location_USA == True) and line != "":
             tempString += line
+    
+    strainDict = removeEmptyStrains(strainDict)
+
+    print("file completed reading...")
+    print('The total amount of data is', len(strainDict))
+    #print(strainDict)
+    return strainDict
+
+#Read the file with fasta files that are in different format and stores the information in dictionary.
+#basic fatsa reader limits for only 225 sequences.
+def readFile2(fileName): 
+    file = open(fileName, 'r')
+    strainDict = {}
+    tempID = ""
+    tempString = "" #fatsa H3N2 DNA strain.
+    seqCount = 0
+    limit = 226
+
+    #read file
+    print("reading file.....")
+    for line in file:
+        if line[0] == '>':
+            seqCount += 1
+            if(tempID != ""):
+                strainDict[tempID] = tempString
+
+            tempID = line
+            strainDict[tempID] = ''
+        else:
+            tempString += line
+        if(seqCount == limit):
+            break
     
     strainDict = removeEmptyStrains(strainDict)
 
@@ -104,3 +137,9 @@ if __name__== "__main__":
     printToFile('CLEAN_RAW_2017_H3N2.fasta', strainDict)
 
     print()
+
+    #third fastfa file
+    strainDict = readFile2('RAW_2018_H3N2_Strains.fasta')
+    printToFile('CLEAN_RAW_2018_H3N2.fasta', strainDict)
+
+
